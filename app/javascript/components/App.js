4cacacc5fb0835e38d9254bb0e03fd4cfe1a74e4
scottduanev2 <adrian.scott.duane@gmail.com>
2020-03-03 11:36:36 -0800

    Initial React component setup

---
+++ app/javascript/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from './Header';
+
+class App extends React.Component {
+  render () {
+    return (
+      <div>Hi there test it out
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ app/javascript/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render () {
+    return (
+      <div>
+        <div>DancePortal</div>
+        <div>A quick look at the best Bay Area dance training</div>
+        <div><a href='http://github.com/scottduanev2/DancePortal'>Github</a></div>
+        <div><a href='adrian.scott.duane@gmail.com'>Email</a></div>
+      </div>
+    )
+  }
+};
+
+export default Header;
